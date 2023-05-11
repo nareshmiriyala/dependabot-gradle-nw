ff259fc697aa54126415f2990f57bd0b789be6d5
Naresh Miriyala <ncmiriyala@gmail.com>
2023-05-11 16:45:49 +1000

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,11 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.4")
+
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("mysql:mysql-connector-java:8.0.25")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
