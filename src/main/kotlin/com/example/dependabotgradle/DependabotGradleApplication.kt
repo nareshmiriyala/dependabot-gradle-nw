dd72d2e72019ef4f365bd8dee1e2c5371ff13f62
Naresh Miriyala <ncmiriyala@gmail.com>
2023-05-11 16:39:03 +1000

    Add ve

---
+++ src/main/kotlin/com/example/dependabotgradle/DependabotGradleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dependabotgradle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DependabotGradleApplication
+
+fun main(args: Array<String>) {
+	runApplication<DependabotGradleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dependabotgradle/DependabotGradleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dependabotgradle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DependabotGradleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
